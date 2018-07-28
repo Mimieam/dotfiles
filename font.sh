544773711ab33343390dd978c9438a42ca177d81
Mimieam <t.echismimi@gmail.com>
2018-07-28 06:29:50 -0500

    clean font

--- font.sh
+++ font.sh
@@ -5,7 +5,6 @@ SRC_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 source $SRC_DIR/set_colors.sh
 source $SRC_DIR/log.sh
 
-msg_config $SRC_DIR/log.sh
 
 function installHermit()
 {
@@ -13,25 +12,24 @@ function installHermit()
     mkdir -p ~/.font/{hermit,tmp}
     # stream to tar without downloading to disk and unzip to the target location :)
     wget -qO - https://pcaro.es/d/otf-hermit-1.21.tar.gz | tar xz -C ~/.font/hermit
-    echo "Donwload and unpacking to ~/.font/hermit"
+    msg_prompt "Donwload and unpacking to ~/.font/hermit"
     rsync -r ~/.font/hermit/ /Users/$USER/Library/Fonts --exclude="LICENSE"
   else
-    res="Font already installed. - Skipping."
-    echo -e  "➜\033[1;33m ${res} \033[0m\n";
-    msg_prompt hi;
+    msg_nested_done "Font already installed. - Skipping."
   fi;
-  echo "Installing to /Users/$USER/Library/Fonts"
+  msg_prompt "Font located in ~/Library/Fonts"
 }
 
+function install_and_apply_font(){
+  msg_prompt "Installing Hermit Font"
+  installHermit
+  msg_done "Done Installing"
+  msg_nested_done "Applying Hermit font"
+  set_font "Hermit medium" 20
+}
 
-echo "Installing Hermit Font"
-installHermit
-msg_done "Done Installing"
-msg_config "Applying font"
-set_font "Hermit medium" 20
+install_and_apply_font
 
-
-if [[ `uname` == 'Darwin' ]]; then
-  msg_done 'Done  Setting up font'
-  # link "$dotfiles/sublime/Packages/User/Preferences.sublime-settings" "$HOME/Library/Application Support/Sublime Text 3/Packages/User/Preferences.sublime-settings"
-fi
+# if [[ `uname` == 'Darwin' ]]; then
+#   # link "$dotfiles/sublime/Packages/User/Preferences.sublime-settings" "$HOME/Library/Application Support/Sublime Text 3/Packages/User/Preferences.sublime-settings"
+# fi
